666d8579835d1b48a52aa56fb065be51b3eb71a6
CGstyling <christinaguseva@gmail.com>
2021-07-20 15:48:56 +0200

    Alles klaar

--- mainSecond.js
+++ mainSecond.js
@@ -221,8 +221,7 @@ function generateTInformation () {
 
 (generateTInformation(inventory));
 
-// BONUS
-// Maak drie knoppen op de pagina: Sorteer op prijs, AmbiLight TV's en Uitverkochte exemplaren. Gebruik de code die je in opdracht 2b, 2c en 2d hebt gemaakt en schrijf dit om naar functies zodat je ze kunt aanroepen op het moment dat de buttons geklikt worden. Zorg ervoor dat de functies de uitkomsten in de de console loggen als de gebruiker op de bijbehorende knop klikt. Tip: Google eens naar het onclick event van buttons!
+
 
 
 
